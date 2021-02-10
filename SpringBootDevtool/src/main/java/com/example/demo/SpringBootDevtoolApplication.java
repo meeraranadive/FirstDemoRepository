850b569a9473abeeb5bc096e05fc5b202b216e91
Hp <Hp@LAPTOP-N5AO8NGE>
2021-02-10 14:38:21 +0530

    123 commit

---
+++ SpringBootDevtool/src/main/java/com/example/demo/SpringBootDevtoolApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevtoolApplication {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello1");
+		System.out.println("meera");
+		System.out.println("devtools");
+		SpringApplication.run(SpringBootDevtoolApplication.class, args);
+	}
+
+}
